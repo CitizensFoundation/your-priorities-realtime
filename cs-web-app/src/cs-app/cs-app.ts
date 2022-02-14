9178b7035a5836f128c92e5e1e44a747aff644ec
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-02-14 03:27:08 +0000

    Work

--- cs-web-app/src/cs-app/cs-app.ts
+++ cs-web-app/src/cs-app/cs-app.ts
@@ -537,7 +537,8 @@ export class CsApp extends YpBaseElement {
       this._onUserChanged({ detail: this.user } as CustomEvent);
     } else if (
       window.location.href.indexOf('/info/') == -1 &&
-      window.location.href.indexOf('/meeting/1') == -1
+      window.location.href.indexOf('/meeting/1') == -1 &&
+      window.location.href.indexOf('/meeting/2') == -1
     ) {
       this.redirectAfterLoginPath = window.location.pathname;
       YpNavHelpers.redirectTo('/login');
